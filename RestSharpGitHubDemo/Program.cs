533fd54433ccbc059721f08219d684c2ccf5e5c6
ilian <ilian78@gmail.com>
2022-05-13 15:52:17 +0300

    Add project files.

---
+++ RestSharpGitHubDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using RestSharp;
+using System;
+
+var client = new RestClient("https://api.github.com");
+var request = new RestRequest("/repos/ilian7811/PostMan/issues");
+var response = await client.ExecuteAsync(request);
+
+Console.WriteLine("BODY : " + response.Content);
+Console.WriteLine("Status code  : " + response.StatusCode);
